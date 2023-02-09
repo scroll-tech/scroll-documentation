468ae58f998a6433e635446291d0d36de14ee069
zzq0826 <770166635@qq.com>
2023-02-09 09:05:07 +0800

    markdown improvement (#3)

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,10 @@ import { youtubeEmbed } from "./integrations/youtube-embed"
 import mdx from "@astrojs/mdx"
 import rehypeSlug from "rehype-slug"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypeKatex from "rehype-katex"
+import remarkGfm from "remark-gfm"
+import remarkMath from "remark-math"
+
 import sitemap from "@astrojs/sitemap"
 
 import tailwind from "@astrojs/tailwind"
@@ -36,12 +40,32 @@ export default defineConfig({
   ],
   markdown: {
     drafts: true,
+    remarkPlugins: [remarkMath, remarkGfm],
     rehypePlugins: [
       rehypeSlug,
       [
         rehypeAutolinkHeadings,
         {
           behavior: "prepend",
+        },
+      ],
+      [
+        rehypeKatex,
+        {
+          macros: {
+            "\\E": "\\mathbb{E}",
+            "\\C": "\\mathbb{C}",
+            "\\R": "\\mathbb{R}",
+            "\\N": "\\mathbb{N}",
+            "\\Q": "\\mathbb{Q}",
+            "\\bigO": "\\mathcal{O}",
+            "\\abs": "|#1|",
+            "\\set": "\\{ #1 \\}",
+            "\\indep": "{\\perp\\mkern-9.5mu\\perp}",
+            "\\nindep": "{\\not\\!\\perp\\!\\!\\!\\perp}",
+            "\\latex": "\\LaTeX",
+            "\\katex": "\\KaTeX",
+          },
         },
       ],
     ],
