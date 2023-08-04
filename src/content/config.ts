533364c69868d70018677ba03cabe81f9d214231
zhengqi zhang <770166635@qq.com>
2023-08-04 17:10:42 +0800

    update docsCollection

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,31 @@ import { defineCollection, z } from "astro:content"
 const docsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    layout: z.string().optional(),
+    image: z
+      .object({
+        src: z.string().optional(),
+        alt: z.string().optional(),
+      })
+      .optional(),
+    dir: z.string().optional(),
+    lang: z.string().optional(),
+    ogLocale: z.string().optional(),
     section: z.string(),
-    // whatsnext: z.string().transform((str) => JSON.parse(str)).optional(),
+    excerpt: z.string().optional(),
+    stub: z.string().optional(),
+    ecosystem: z.string().optional(),
+    l2healthflag: z.string().optional(),
+
+    metadata: z
+      .object({
+        title: z.string().optional(),
+        description: z.string().optional(),
+        // image: z.array(z.string()).optional(),
+      })
+      .optional(),
+
+    whatsnext: z.record(z.string()).optional(),
   }),
 })
 
