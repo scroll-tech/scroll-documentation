6aec5186a8c4bac337567609970dae1069a06b34
ahmeteme <90158882+ahmeteme@users.noreply.github.com>
2024-01-06 17:33:17 +0300

    Update linkcheck.ts

--- scripts/linkcheck.ts
+++ scripts/linkcheck.ts
@@ -1,54 +1,64 @@
-import { platform, exit, argv, stdout } from "process"
+import { platform, exit, argv, stdout } from "process";
+import { spawn } from "child_process";
 
-import { spawn } from "child_process"
+const PORT = ":3000";
+const IGNORE_FILES_PATH = `${process.cwd()}/scripts/ignoredfiles.txt`;
 
 const server = spawn("npm", ["run", "preview"], {
   stdio: ["ignore", "pipe", "ignore"],
-})
+});
 
-let external = false
-if (argv[argv.length - 1] === "-e") {
-  external = true
+let external = false;
+if (argv.includes("-e")) {
+  external = true;
 }
 
-let output = ""
+let output = "";
 server.stdout.on("data", (data) => {
-  stdout.write(data.toString())
-  output = output + data.toString()
-  if (output.indexOf("use --host to expose") !== -1) {
-    let program
-    if (platform === "linux") {
-      program = "linkcheck-linux"
-    } else if (platform === "darwin") {
-      program = "linkcheck"
-    } else {
-      program = "linkcheck-win"
-    }
-    console.log(__dirname, process.cwd())
-    const args = [":3000", "--skip-file", `${process.cwd()}/scripts/ignoredfiles.txt`]
+  const dataStr = data.toString();
+  stdout.write(dataStr);
+  output += dataStr;
+
+  if (output.includes("use --host to expose")) {
+    const program = getProgramName(platform);
+    console.log(__dirname, process.cwd());
+
+    const args = [PORT, "--skip-file", IGNORE_FILES_PATH];
     if (external) {
-      args.push("-e")
-      console.log("Also checking external links.")
+      args.push("-e");
+      console.log("Also checking external links.");
     }
+
     const checker = spawn(program, args, {
       stdio: ["ignore", "pipe", "ignore"],
-    })
+    });
+
     checker.stdout.on("data", (checkerData) => {
-      stdout.write(checkerData.toString())
-    })
-    checker.on("exit", (code, other) => {
-      console.log("linkcheck finished")
-      server.stdout.destroy()
-      server.kill()
-      checker.stdout.destroy()
-      checker.kill()
-      let result
-      if (code === null) {
-        result = undefined
-      } else {
-        result = code
-      }
-      exit(result)
-    })
+      stdout.write(checkerData.toString());
+    });
+
+    checker.on("exit", handleCheckerExit);
   }
-})
+});
+
+function getProgramName(platform) {
+  switch (platform) {
+    case "linux":
+      return "linkcheck-linux";
+    case "darwin":
+      return "linkcheck";
+    default:
+      return "linkcheck-win";
+  }
+}
+
+function handleCheckerExit(code, other) {
+  console.log("linkcheck finished");
+  server.stdout.destroy();
+  server.kill();
+  checker.stdout.destroy();
+  checker.kill();
+
+  const result = code === null ? undefined : code;
+  exit(result);
+}
