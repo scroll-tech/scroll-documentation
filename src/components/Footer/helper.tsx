fde811ebbf380f1413889e313c13df233cfd88af
Daniel Helm <dan.helm@gmail.com>
2024-01-15 17:30:04 -0600

    fix merge issue with brand kit

--- src/components/Footer/helper.tsx
+++ src/components/Footer/helper.tsx
@@ -54,7 +54,7 @@ export const resourceList = [
     href: "https://docs.scroll.io/",
   },
   {
-    name: "footer.resources.pressKit",
+    name: "footer.resources.brandKit",
     href: "https://scrollzkp.notion.site/Scroll-Rebrand-Assets-5bb83465f56f40989c4f772b39ed3a06",
   },
 ]
