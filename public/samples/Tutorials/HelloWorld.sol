be36fd91426c3c57bdb1e07353636e0926e46e49
Jan <93680948+dcjanio@users.noreply.github.com>
2024-03-26 01:42:58 +0100

    Update HelloWorld.sol
    Added function called getMessage()
    and added additional documentation

--- public/samples/Tutorials/HelloWorld.sol
+++ public/samples/Tutorials/HelloWorld.sol
@@ -9,12 +9,25 @@ pragma solidity 0.8.7;
 
 contract HelloWorld {
     string public message;
-
+/**
+    * @dev Constructor that sets the initial message.
+    * @param initialMessage The initial message to be stored in the contract.
+*/
     constructor(string memory initialMessage) {
         message = initialMessage;
     }
-
+/**
+     * @dev Updates the message stored in the contract.
+     * @param newMessage The new message to replace the current one.
+*/
     function updateMessage(string memory newMessage) public {
         message = newMessage;
+    }
+/**
+     * @dev Retrieves the current message stored in the contract.
+     * @return The current message.
+*/
+    function getMessage() public view returns (string memory) {
+        return message;
     }
 }
